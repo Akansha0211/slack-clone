ae0709f1f3244181f60241a32f81b304fd6e5316
Akansha0211 <akansha.saxena0211@gmail.com>
2021-04-18 19:03:02 +0530

    Add routing

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Creating a new project
 
-...
 npx create-react-app project-name
-...
+
 
 # Starting a project
 
-...
+
 npm start
-...
+
+
+# Components 
+-- has index.js file , which imports all the components and exports them
+
+# React routers 
+ npm i react-router-dom 
+
+ Routing our app on the client side( url and page changed withour w=evn refreshing the page)
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {SignIn} from './';
+import{Switch, Route} from 'react-router-dom';
+
+function Home(){
+  return (
+    <div>Home</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      Hello World
-      <SignIn/>
+      <Switch>
+        <Route exact path="/" component={SignIn}/>
+        <Route exact path="/home" component={Home}/>
+      </Switch>
     </div>
   );
 }
