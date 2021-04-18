2be890e66a18e698437d6660691f0fa5bad4d0ed
Akansha0211 <akansha.saxena0211@gmail.com>
2021-04-18 18:33:34 +0530

    SignIn styling

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,11 +3,12 @@ import React, {Component} from 'react';
 class SignIn extends Component{
     render(){
         return(
-            <div>
-                SignIn
+            <div className="signin-form">
+                <h1>SignIn/Sign Up</h1>
+                <button>Sign in with Google/Signup with Google</button>
             </div>
         );
     }
-}
+}  
 
 export default SignIn;
